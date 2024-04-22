ea4907872320658dc183ed7060a7193cb0d58cfd
siyaphe <siyapdje@yahoo.fr>
2024-04-21 21:40:31 -0400

    Express server Application Final Commit

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express();
+
+const dessertsRoute = require('./routes/desserts');
+const foodsRoute = require('./routes/foods');
+const drinksRoute = require('./routes/drinks');
+const desserts = require('./data/desserts');
+//----------------------------------[Importing route handlers]
+// let {desserts} = require('./da?ta')
+
 const PORT = process.env.PORT || 5000;
 // ---------------------------------------------[express Application]
+
+app.use(express.static('public'));
+//-------------------------------------Static and middleware
+
+app.use(express.urlencoded({ extended: false }));
+//--------------------------------------[Parse Data]
+
+app.use(express.json());
+//---------------------------------------[Middleware - parse JSON]
 
 const logReq = (req, res, next) => {
     console.log(` ${req.method} ${req.url}`);
@@ -10,26 +27,20 @@ const logReq = (req, res, next) => {
 };
 // ---------------------------Middleware to log requests
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(logReq);
-
-//-------------------------- Middleware - parse JSON bodies
+//--------------------------  bodies
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.use(express.static('public'));
 app.engine('ejs', require('express'));
+
 //--------------------- [view engine - directory for views]
 
-const dessertsRoute = require('./routes/desserts');
-const foodsRoute = require('./routes/foods');
-const drinksRoute = require('./routes/drinks');
-//----------------------------------[Importing route handlers]
 
-app.use('/desserts', dessertsRoute);
-app.use('/foods', foodsRoute);
-app.use('/drinks', drinksRoute);
+
+app.use('/api/desserts', dessertsRoute);
+app.use('/api/foods', foodsRoute);
+app.use('/api/drinks', drinksRoute);
 //------------------- [Routes for desserts, foods, and drinks]
 
 app.use((err, req, res, next) => {
@@ -39,10 +50,57 @@ app.use((err, req, res, next) => {
 //-------------------------- [Errorhandling middleware]
 
 
-app.get('/desserts', (req, res) => {
+app.get('api/desserts', (req, res) => {
     res.render('desserts');
 });
-//-------------------------------- [Route desserts view]
+//-----------------------------------------------[GET request.]
+
+app.post('api/desserts/', (req, res) => {
+    const { name } = req.body
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, msg: 'please provide name value' })
+    }
+    res.status(201).json({ success: true, data: [...desserts,name]})
+  })
+//-----------------------------------------------[POST request.]
+
+  app.put('/api/desserts/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+  
+    const dessert = desserts.find((d) => d.id === Number(id))
+  
+    if (!desserts) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `no person with id ${id}` })
+    }
+    const dessertsRoute = desserts.map((dessert) => {
+      if (dessert.id === Number(id)) {
+        dessert.name = name
+      }
+      return dessert
+    })
+    res.status(200).json({ success: true, data:dessertsRoute})
+  })
+//-----------------------------------------------[PUT request.]
+
+  app.delete('/api/desserts/:id', (req, res) => {
+    const dessert = desserts.find((d) => d.id === Number(req.params.id))
+    if (!desserts) {
+      return res
+        .status(404)
+        .json({ success: false, msg: `no dessert with id ${req.params.id}` })
+    }
+    const dessertsRoute = desserts.filter(
+      (dessert) => dessert.id !== Number(req.params.id)
+    )
+    return res.status(200).json({ success: true, data:dessertsRoute})
+  });
+//-----------------------------------------------[DELETE request.]
+//------------------------------------------------ [Route desserts view]
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
